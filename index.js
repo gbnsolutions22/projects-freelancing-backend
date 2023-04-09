d8a2dece29b13672b3f7bff82828ebbcb0c41f29
gbnsolutions22 <gbnsolutions.22@gmail.com>
2023-04-09 09:51:49 +0530

    cors changed

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const connectDB = async ()=> {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:8080"],
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   })
